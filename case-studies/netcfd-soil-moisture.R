b19f68d270c29faba93a5b99754377e22155d508
Peter Prevos <peter@prevos.net>
2025-06-23 11:01:25 +1000

    Updated soil moisture visualisations

--- case-studies/netcfd-soil-moisture.R
+++ case-studies/netcfd-soil-moisture.R
@@ -1,57 +1,68 @@
-# Analysing soil moisture data with the ncdf4 library
+## Analysing soil moisture data with the ncdf4 library
 
-# Reading, Extracting and Transforming with the ncdf4 library
+## Reading, Extracting and Transforming with the ncdf4 library
 library(ncdf4)
 
-bom <- nc_open("data/ss_pct_Actual_month.nc")
+bom <- nc_open("data/sm_pct.nc")
 
-print(bom) # Inspect the data
+## Generate matrix
+longitude <- ncvar_get(bom, "longitude")
+latitude <- ncvar_get(bom, "latitude")
+month <- as.Date("1900-01-01") + ncvar_get(bom, "time")
 
-lon <- ncvar_get(bom, "longitude")
-lat <- ncvar_get(bom, "latitude")
-t <- as.Date("1900-01-01") + ncvar_get(bom, "time")
+moisture <- ncvar_get(bom, "sm_pct")
+dimnames(moisture) <- list(longitude, latitude, month)
 
-moisture <- ncvar_get(bom, "ss_pct")
-dimnames(moisture) <- list(lon, lat, t)
-
-# Visualising the data with ggplot
-library(tidyverse)
-library(RColorBrewer)
+## Visualising the data with ggplot
+library(ggplot2)
+library(ozmaps)
 library(reshape2)
 
-d <- "2017-07-31"
-m <- moisture[, , which(t == d)] %>%
-  melt(varnames = c("lon", "lat")) %>%
+plot_date <- "2025-05-31"
+
+## Generate data frame
+soil_moisture <- moisture[, , which(month == plot_date)] %>%
+  melt(varnames = c("longitude", "latitude")) %>%
   subset(!is.na(value))
 
-ggplot(m, aes(x = lon, y = lat, fill = value)) + 
-  borders("world") +
-  geom_tile() +
-  scale_fill_gradientn(colors = brewer.pal(9, "Blues")) +
-  labs(title = "Total moisture in deep soil layer (100-500 cm)",
-       subtitle = format(as.Date(d), "%d %B %Y")) +
-  xlim(range(lon)) + 
-  ylim(range(lat)) + 
-  coord_fixed()
+## State boundaries
+sf_aus <- ozmap("states", add = TRUE)
 
+ggplot(soil_moisture) + 
+  geom_tile(aes(x = longitude, y = latitude, fill = value)) +
+  scale_fill_gradient(low = "chocolate4", high = "dodgerblue", name = NULL) + 
+  geom_sf(data = sf_aus, fill = NA, col = "white") + 
+  labs(title = "Relative Total rootzone soil moisture (0-100 cm)",
+       subtitle = format(as.Date(plot_date), "%d %B %Y"),
+       caption = "Source: Australian Landscape Water Balance AWRA-L Model") + 
+  theme_void()
+
+## Plot on Google Maps
 library(ggmap)
+
 api <- readLines("case-studies/google-maps.api") # Text file with the API key
 register_google(key = api)
 
-loc <- round(geocode("Bendigo") / 0.05) * 0.05 
+ggmap(get_map("Australia", zoom = 4, color = "bw")) +
+  geom_tile(data = soil_moisture, 
+            aes(x = longitude, y = latitude, fill = value), alpha = 0.5) +
+  scale_fill_gradient(low = "chocolate4", high = "dodgerblue", name = NULL) + 
+  labs(title = "Relative Total rootzone soil moisture (0-100 cm)",
+       subtitle = format(as.Date(plot_date), "%d %B %Y"),
+       caption = "Source: Australian Landscape Water Balance AWRA-L Model") + 
+  theme_void()
 
-get_map(loc, zoom = 12) %>%
-  ggmap() +
-  geom_tile(data = m, aes(x = lon, y = lat, fill = value), alpha = 0.5) +
-  scale_fill_gradientn(colors = brewer.pal(9, "Blues")) +
-  labs(title = "Total moisture in deep soil layer (100-500 cm)",
-       subtitle = format(as.Date(d), "%d %B %Y"))
+## Time series
+location <- round(geocode("Bendigo, Australia") / 0.05) * 0.05 
 
-mt <- data.frame(date = t,
-                 dp = moisture[as.character(loc$lon), as.character(loc$lat), ])
+soil_moisture_time <- tibble(month,
+                             moisture = moisture[as.character(location$lon), 
+                                                 as.character(location$lat), ]) %>% 
+  filter(month >= "2000-01-01")
 
-ggplot(mt, aes(x = t, y = dp)) + geom_line() +
-  labs(x = "Month",
-       y = "Moisture",
-       title = "Total moisture in deep soil layer (100-500 cm)",
-       subtitle = paste(as.character(loc), collapse = ", "))
+ggplot(soil_moisture_time) + 
+  geom_line(aes(x = month, y = moisture)) +
+  labs(title = "Relative Total rootzone soil moisture (0-100 cm)",
+       subtitle = paste(as.character(location), collapse = ", "),
+       caption = "Source: Australian Landscape Water Balance AWRA-L Model") + 
+  theme_minimal(base_size = 16)

013d384b67cd579a9cd310e23e2f345be737287c
Peter Prevos <peter@prevos.net>
2025-12-15 21:27:25 +1100

    Updated bubble chart script

--- case-studies/bubble-chart.R
+++ case-studies/bubble-chart.R
@@ -9,7 +9,7 @@ summary(water$Consumption)
 ## Plot on Google Maps
 library(ggmap)
 
-api <- readLines("case-studies/google-maps.api") # Text file with the API key
+api <- readLines("data/google-maps.api") # Text file with the API key
 register_google(key = api)
 
 centre <- c(mean(range(water$lon)), mean(range(water$lat)))
@@ -17,9 +17,8 @@ viettri <- get_map(centre, zoom = 17, maptype = "hybrid")
 g <- ggmap(viettri)
 
 # Add data to ggmap
-
 g + geom_point(data = water, aes(x = lon, y = lat, size = Consumption),
                shape = 21, colour = "dodgerblue4", fill = "dodgerblue", 
                alpha = .8) +
-  scale_size_area(max_size = 30) + # Size of the biggest point
+  scale_size_area(max_size = 12) + # Size of the biggest point
   ggtitle("Việt Trì sự tiêu thụ nước")
